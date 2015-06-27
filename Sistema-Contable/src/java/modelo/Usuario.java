84cb170e323f31f1c06404561abd1b4ec20157e3
Erick Gonález <Erick Gonlez@10.152.3.212>
2015-06-26 18:11:28 -0600

    SegundaPrueba

---
+++ Sistema-Contable/src/java/modelo/Usuario.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Erick Gonález
+ */
+@ManagedBean
+@RequestScoped
+public class Usuario {
+
+    /**
+     * Creates a new instance of Usuario
+     */
+    public Usuario() {
+    }
+    
+}
